94201aea89eb369860d2cb735e4158f2e0a1077b
Where456 <daanagrigoreva1@gmail.com>
2023-12-21 15:32:36 +0300

    Initial commit

---
+++ main_app/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from main_app.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price_per_unit', 'category',)
+    list_filter = ('name', 'description', 'category',)
+
+
---
+++ main_app/management/commands/fill_p.py
@@ -0,0 +1,23 @@
+import json
+
+from django.core.management import BaseCommand
+
+from main_app.models import Product
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        Product.objects.all().delete()
+
+        products_list = []
+        with open('main_app/fixtures/products.json', encoding='utf-8') as f:
+            categories = json.load(f)
+        for product_item in categories:
+            if len(products_list) < 12:
+                products_list.append(product_item["fields"])
+            else:
+                break
+        print(products_list)
+        for product_item in products_list:
+            Product.objects.create(**product_item)
